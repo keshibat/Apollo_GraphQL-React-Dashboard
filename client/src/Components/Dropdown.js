490cae43ad596a92f88607d26c505b6e2c1f255e
Kensuke Shibata <kensukeshibata@gmail.com>
2019-09-06 10:15:39 +1000

    for the time being

--- client/src/Components/Dropdown.js
+++ client/src/Components/Dropdown.js
@@ -3,31 +3,39 @@ import { Dropdown } from "semantic-ui-react";
 import { graphql } from "react-apollo";
 import { getDashboardAllDataQuery } from "../queries/queries";
 
+const carOptions = [
+  {
+    key: "Honda",
+    text: "Honda",
+    value: "Honda"
+  },
+  {
+    key: "Toyota",
+    text: "Toyota",
+    value: "Toyota"
+  },
+  {
+    key: "Mercedez Benz",
+    text: "Mercedez Benz",
+    value: "Mercdez Benz"
+  }
+];
+
 class CarDropDown extends Component {
   render() {
+    const carLists = this.props.data.owner.cars;
+    console.log(carLists);
+
+    const ownerCarList = carLists.map(car => {
+      return { key: car.carName, text: car.carName, value: car.carName };
+    });
     return (
       <Dropdown
         className="dd-styles"
         placeholder="Select Car"
         fluid
         selection
-        options={[
-          {
-            key: "Honda",
-            text: "Honda",
-            value: "Honda"
-          },
-          {
-            key: "Toyota",
-            text: "Toyota",
-            value: "Toyota"
-          },
-          {
-            key: "Mercedez Benz",
-            text: "Mercedez Benz",
-            value: "Mercdez Benz"
-          }
-        ]}
+        options={ownerCarList}
       />
     );
   }
--- client/src/Components/Sample.js
+++ client/src/Components/Sample.js
@@ -2,36 +2,18 @@ import React, { Component } from "react";
 import { graphql } from "react-apollo";
 import { getDashboardAllDataQuery } from "../queries/queries";
 
-const carOptions = [
-  {
-    key: "Honda",
-    text: "Honda",
-    value: "Honda"
-  },
-  {
-    key: "Toyota",
-    text: "Toyota",
-    value: "Toyota"
-  },
-  {
-    key: "Mercedez Benz",
-    text: "Mercedez Benz",
-    value: "Mercdez Benz"
-  }
-];
-
 class Sample extends Component {
   render() {
     const carLists = this.props.data.owner.cars;
-    console.log(carLists);
-
     const ownerCarList = carLists.map(car => {
-      return car.carName;
+      return <li>{car.carName}</li>;
     });
 
     return (
       <>
-        <h1>{ownerCarList}</h1>
+        <ul>
+          <li>{ownerCarList}</li>
+        </ul>
       </>
     );
   }
